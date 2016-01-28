24a9ffd03a69be69a668ea8452746d371df0be3a
Haoran Liu <ymiku@qq.com>
2016-01-28 19:24:31 +0800

    PoolManager
    for unity

---
+++ LCGameObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class LCGameObject : MonoBehaviour,IPoolable {
+	public virtual void Destroy()
+	{
+		if (!PoolManager.Instance.IsPoolFull(GetType()))
+		{
+			PoolManager.Instance.PutPoolObject(GetType(), this);
+		}
+		else
+		{
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
